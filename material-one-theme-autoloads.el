61f6bee7f3e70d9c8870048748c1631e6968f66d
Lafwind <lafwind@gmail.com>
2015-08-10 17:24:06 +0800

    Initial commit

---
+++ material-one-theme-autoloads.el
@@ -0,0 +1,8 @@
+;;; material-one-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;; material-one-theme-autoloads.el ends here
---
+++ material-one-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "material-one-theme" "20150810.1231" "A Theme " '((emacs "24.1")) :url "http://github.com/" :keywords '("themes"))
