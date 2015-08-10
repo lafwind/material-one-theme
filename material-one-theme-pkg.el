9658522daecda3c240520133e2c78334a8bc5dd4
Lafwind <lafwind@gmail.com>
2015-08-10 17:54:53 +0800

    select color

--- material-one-theme-pkg.el
+++ material-one-theme-pkg.el
@@ -1,2 +1,2 @@
 ;;; -*- no-byte-compile: t -*-
-(define-package "material-one-theme" "20150810.1231" "A Theme " '((emacs "24.1")) :url "http://github.com/" :keywords '("themes"))
+(define-package "material-one-theme" "20150810.1231" "A Theme inspired by material" '((emacs "24.1")) :url "http://github.com/lafwind/" :keywords '("themes"))
